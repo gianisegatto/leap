39d516bf6a1597998ac5bcdf6796aab7d706c8cf
Giani Segatto <gsegatto@travix.com>
2019-05-04 17:54:29 +0100

    reading directory recursively

--- Index.js
+++ Index.js
@@ -4,6 +4,6 @@ const start = new Date().getTime();
 
 const contextLoader = new ContextLoader();
 
-contextLoader.main();
+contextLoader.load();
 
 console.log("Context loaded in: " + (new Date().getTime() - start) + " ms");
