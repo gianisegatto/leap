b0200ee76d50bd3b8a2f1ad118040f890a5ff6ee
Giani Segatto <gsegatto@travix.com>
2019-05-12 12:14:07 +0100

    moving the error message to the validator

--- src/main/node/ContextLoader.js
+++ src/main/node/ContextLoader.js
@@ -18,23 +18,14 @@ class ContextLoader {
         const instances = this.componentFactory.createInstances(components);
 
         const missingInstances = this.postValidator.validate(instances);
-        if (missingInstances.length > 0) {
-            console.error("Oh Oh 💩");
-            console.error("Something went wrong mate. The context loader failed to load " + missingInstances.length + " component(s)");
-            missingInstances.forEach(component => {
-                console.error("Cannot create component: " + component.getFullPath())
-                component.getMissingParams().forEach(param => console.log("Cannot find parameter: " + param));
-            });
+        if (!missingInstances) {
+            this.postValidator.printMessage();
             process.exit(1);
         } else {
             console.log("Cheers mate leap is up and running 🍻");
         }
             
-        
-
-        // const component = instances["SomeController"];
-
-        // component.run();
+        // console.log(JSON.stringify(instances));
     }
 }
 
--- src/main/node/validator/PostValidator.js
+++ src/main/node/validator/PostValidator.js
@@ -1,17 +1,33 @@
-function filterNoInstanceAndAttempts(component, numberOfComponents) {
-    return component.getCreationAttempts() >= numberOfComponents && component.getInstance() === null;
-}
-
 class PostValidator {
+
+    constructor() {
+        this.missingInstances;
+    }
 
     validate(components) {
 
-        const numberOfComponents = components.length;
-
-        return components
-            .filter(component => filterNoInstanceAndAttempts(component, numberOfComponents));
+        this.missingInstances = components
+            .filter(component => filterNoInstanceAndAttempts(component, components.length));
+        
+        if (this.missingInstances.length > 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
+    printMessage() {
+        console.error("Oh Oh 💩");
+        console.error("Something went wrong mate. The context loader failed to load " + missingInstances.length + " component(s)");
+        missingInstances.forEach(component => {
+            console.error("Cannot create component: " + component.getFullPath())
+            component.getMissingParams().forEach(param => console.log("Cannot find parameter: " + param));
+        });
+    }
 }
 
 module.exports = PostValidator;
+
+function filterNoInstanceAndAttempts(component, numberOfComponents) {
+    return component.getCreationAttempts() >= numberOfComponents && component.getInstance() === null;
+};
